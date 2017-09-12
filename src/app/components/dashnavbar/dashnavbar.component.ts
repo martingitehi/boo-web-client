61411061eab8bacccd3d2c84f29d3603f3852c10
Martin Gitehi <martingitehi@live.com>
2017-09-12 04:17:38 +0300

    initial commit

--- src/app/components/dashnavbar/dashnavbar.component.ts
+++ src/app/components/dashnavbar/dashnavbar.component.ts
@@ -9,6 +9,7 @@ import { Location } from '@angular/common';
 })
 export class DashnavbarComponent implements OnInit {
 filter:any = '';
+showSearch:boolean=true;
   constructor(private api:API, private location:Location) { }
 
   ngOnInit() {
