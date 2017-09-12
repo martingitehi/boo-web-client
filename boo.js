305b9a24e3170cd83e9d71fcdb5ab3e073746478
Martin Gitehi <martingitehi@live.com>
2017-09-12 04:25:28 +0300

    initial commit

--- boo.js
+++ boo.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const path = require('path');
-
+app.use(cors());
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', (req, res)=>{
